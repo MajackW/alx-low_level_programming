f2766a2aa6c59a2764f29d14b3c697d673ff78cf
majack wariaka <majackwariaka@gmail.com>
2023-08-10 12:37:51 -0700

    low level programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-	/*
-	 *Description: this program prints a message
-	 * main function returns 0
-	 */
+/*
+ *Description: this program prints a message
+ * main function returns 0
+ */
 int main(void)
 {
-	/*prints characters*/
+/*prints characters*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	/*returns 0*/
+/*returns 0*/
 	return (0);
 }
