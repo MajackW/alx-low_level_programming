e7abb423b8cce1b448fd73abe64b6a0d7771ba86
majack wariaka <majackwariaka@gmail.com>
2023-08-11 03:21:00 -0700

    conditionals

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	char A;
+
+	a = 'a';
+	B = 'B';
+
+	while (a <= 'z')
+	{
+		putchar('a');
+		a++;
+	}
+
+	while (A <= 'Z')
+	{
+		putchar('A');
+		A++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
