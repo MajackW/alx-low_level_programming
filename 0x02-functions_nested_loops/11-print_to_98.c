bf15dbfbb9d374ed90dc3007b7065b8d96411e93
majack wariaka <majackwariaka@gmail.com>
2023-08-15 09:41:10 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+/**
+ * n - number
+ */
+{
+	if (n < 0)
+	{
+		printf("%d, ", n);
+		n++;
+		while (n < 99)
+		{
+			if (n != 98)
+			{
+				printf("%d, ", n);
+			}
+			else if (n == 98)
+			{
+				printf("%d", n)
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+	        printf("%d, ", n);
+                n--;
+                while (n < 97)
+                {
+                        if (n != 98)
+                        {
+                                printf("%d, ", n);
+                        }
+                        else if (n == 98)
+                        {
+                                printf("%d", n)
+                        }
+                        n--;
+                }
+	}
+	else if (n == 98)
+	{
+		printf("%d", n)
+	}
+}
