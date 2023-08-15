286168046056e9d7be733d269c437dbabd5ce347
majack wariaka <majackwariaka@gmail.com>
2023-08-15 07:37:31 -0700

    nested loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_last_digit - Entry point
+ *
+ * Return: returns the last digit
+ */
 int print_last_digit(int n)
 {
 	int ret;
 
 	ret = n % 10;
+
+	putchar(ret);
 
 	return (ret);
 }
