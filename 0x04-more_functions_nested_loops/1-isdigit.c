652ef7bbafa302f5201870e8ca9b793fcd355c1e
majack wariaka <majackwariaka@gmail.com>
2023-08-17 10:33:58 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * @c: character
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
