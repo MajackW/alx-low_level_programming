bb377d8123d047f9f2469752775cd57518eca8f4
majack wariaka <majackwariaka@gmail.com>
2023-08-17 12:59:42 -0700

    functions'

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,13 +15,14 @@ void print_square(int size)
 	int b;
 
 	a = 1;
+	if (size <= 0)
+	{
+		putchar('\n');
+		return;
+	}
 	while (a <= size)
 	{
-		if (size <= 0)
-		{
-			putchar('\n');
-			return;
-		}
+
 		b = 1;
 		while (b <= size)
 		{
