741a9a165c47ff175acbfd4872584fcef316f8d7
majack wariaka <majackwariaka@gmail.com>
2023-08-17 10:41:49 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: first num
+ * @b: second num
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
