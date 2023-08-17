108a061e3e599d2d80da7408d0997cd509986fa0
majack wariaka <majackwariaka@gmail.com>
2023-08-17 10:19:29 -0700

    more functiions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ *@c: char c
+ * Return: 1 0r 0
+ */
+
+int _isupper(int c):
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
