b2943c8d4992638119db4958f7cdfa90961b5116
majack wariaka <majackwariaka@gmail.com>
2023-08-21 15:14:18 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: copy
+ * @src: copy
+ * Return: char address
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p;
+
+	p = dest;
+	dest = src;
+
+	return (p);
+}
