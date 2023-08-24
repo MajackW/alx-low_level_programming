0c5b1870ecd61a68bb23445b0a3279beb74be8c4
majack wariaka <majackwariaka@gmail.com>
2023-08-24 10:36:03 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcmp - compares strings
+ * @s1: first string
+ * @s2: second tstring
+ * Return: int
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int cs1;
+	int cs2;
+
+	cs1 = 0;
+	cs2 = 0;
+	while (*s1 != '\0')
+	{
+		cs1++;
+		s1++
+	}
+	while (*s2 != '\0')
+	{
+		cs2++;
+		s2++;
+	}
+	if (cs1 == cs2)
+	{
+		return (0);
+	}
+	else if (cs1 > cs2)
+	{
+		return (15);
+	}
+	else if (cs2 > cs1)
+	{
+		return (-15);
+	}
+}
