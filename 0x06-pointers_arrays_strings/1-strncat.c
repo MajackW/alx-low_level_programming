ce10946214bce1f03b3ad86dda283998f20903fb
majack wariaka <majackwariaka@gmail.com>
2023-08-24 09:47:50 -0700

    strcat part 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *original = dest;
+
+	while (*dets != '\0')
+	{
+		dest++;
+	}
+	while (*src != 0 && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n__;
+	}
+	*dest = '\0';
+	return (original);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,4 +5,6 @@ void _putchar(void);
 
 char *_strcat(char *dest, char *src);
 
+char *_strncat(char *dest, char *src, int n);
+
 #endif
