265b914f25adbf4a1f677010edf2ec807fbbd956
majack wariaka <majackwariaka@gmail.com>
2023-08-25 00:47:27 -0700

    capitalize first letters

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * cap_string - capitalize letters
+ * @str: string to capitalize
+ * Return: char capitalized string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - ('a' - 'A');
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '.')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - ('a' - 'A');
+			}
+		}
+		else
+			i++;
+	}
+	return (str);
+}
