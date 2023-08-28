ae7134a1f82961ed72b18f7cf2b5d2ee3811b9c4
Majack <majackwariaka@gmail.com>
2023-08-28 11:28:41 -0700

    occurence of a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strpbrk - searches for a string of a set of bytes
+ * @s: pointer to main string
+ * @accept: pointer to string to search
+ * Return: pointer to found occurence
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *a;
+	while (*s != '\0')
+	{
+		a = accept;
+		while (a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,4 +7,6 @@ char *_strstr(char *haystack, char *needle);
 
 void print_chessboard(char (*a)[8]);
 
+char *_strpbrk(char *s, char *accept);
+
 #endif
