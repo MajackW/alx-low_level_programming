538aef5981afc54ac560cd57d821ea86c6fb7c82
Majack Wariaka <majackwariaka@gmail.com>
2023-09-05 18:07:56 +0300

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,20 @@
 
 char *create_array(unsigned int size, char c)
 {
+	int i;
 	char *str;
 
 	if (size == 0)
 		return (NULL);
 
-	str = (char *)malloc(sizeof(str) * size);
+	str = (char *)malloc((sizeof(char) * size) + 1);
 	if (str == NULL)
 		return (NULL);
-	str[0] = c;
+	for (i = 0; i < size; i++)
+	{
+		str[i] = c;
+	}
+	str[size] = '\0';
 
 	return (str);
 }
