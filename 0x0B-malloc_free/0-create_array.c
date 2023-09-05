c1d7800d4c63708540d87bb5952d5b25251d84f9
Majack Wariaka <majackwariaka@gmail.com>
2023-09-05 17:54:39 +0300

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - ebtry point
+ * @size: size of buffer
+ * @c: char to intialize
+ * Return: pointer to buffer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	str = (char *)malloc(sizeof(str) * size);
+	if (str == NULL)
+		return (NULL);
+	str[0] = c;
+
+	return (str);
+}
