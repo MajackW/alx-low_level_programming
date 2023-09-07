f4e2767e4ede47dce58cd13de7669b7e91337ee7
Majack Wariaka <majackwariaka@gmail.com>
2023-09-07 20:41:37 +0300

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,16 +13,18 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
+	unsigned i;
 	unsigned l1;
 	unsigned l2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = "";
+		s2 == "";
 	l1 = strlen(s1);
-	l2 = strlen(s2);
-	p = (char *)malloc(sizeof(char) *(l1 + n) + 1);
+	l2 = strlen(s1);
+
+	p = (char *)malloc(l1 + n + 1);
 	if (p == NULL)
 		return (NULL);
 	strcpy(p, s1);
@@ -31,9 +33,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		strcat(p, s2);
 		return (p);
 	}
-	for (l1 = l1; l1 < l1 + n; l1++)
+	for (i = l1; i < l1 + n; i++)
 	{
-		p[l1] = *s2;
+		p[i] = *s2;
 		s2++;
 	}
 	p[l1 + n] = '\0';
