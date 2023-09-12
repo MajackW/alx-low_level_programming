0694e7627ec37820190970e833fe3da86cf083af
Majack Wariaka <majackwariaka@gmail.com>
2023-09-12 18:05:59 +0300

    spacing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,18 +11,18 @@ void print_dog(struct *d)
 	if (d == NULL)
 		return;
 	printf("Name: ");
-	if (d -> name == NULL)
+	if (d->name == NULL)
 		printf("(nil)\n");
-	else if (d -> name != NULL)
-		printf("%s\n", d -> name);
+	else if (d->name != NULL)
+		printf("%s\n", d->name);
 	printf("Age: ");
-	if (d -> age == NULL)
+	if (d->age == NULL)
 		printf("(nil)\n");
-	else if (d -> != NULL)
-		printf("%d\n", d -> age);
+	else if (d->!= NULL)
+		printf("%d\n", d->age);
 	printf("Owner: ");
-	if (d -> owner == NULL)
+	if (d->owner == NULL)
 		printf("(nil)\n");
-	else if (d -> owner != NULL)
-		printf("%s\n", d -> owner);
+	else if (d->owner != NULL)
+		printf("%s\n", d->owner);
 }
