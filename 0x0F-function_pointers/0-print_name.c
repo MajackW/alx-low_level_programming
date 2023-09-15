bf3bd1bc6f1e01648e74bd49460cb7b74d41da07
Majack Wariaka <majackwariaka@gmail.com>
2023-09-15 19:24:55 +0300

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
-
+/**
+ * print_name - entry point
+ * @name: name
+ * @f: pointer
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
