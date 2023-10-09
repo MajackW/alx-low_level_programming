03ccda00062a51df0311d23fbdaa792acb2c2afb
Majack <majackwariaka@gmail.com>
2023-10-09 02:55:19 -0700

    comments for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - defines dog with name owner age
+ * @name: name of dog
+ * @age: age of dogh
+ * @owner: owne of the dog
+ */
 struct dog
 {
 	char *name;
