7fd462a918da3e05784b14269d4ee46569a1830f
Majack <majackwariaka@gmail.com>
2023-10-09 01:30:50 -0700

    macro calculating sum

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef 3_FUNCTION_LIKE_MACRO_H
+#define 3_FUNCTION_LIKE_MACRO_H
+#define ABS(X) ((X) < (0) ? -(X) : (X))
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,4 @@
+#ifndef 4_SUM_H
+#define 4_SUM_H
+#define SUM(X, Y) (X) + (Y)
+#endif
