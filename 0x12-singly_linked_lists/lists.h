305dcf46090c530f21a3d5bd2729fe1be1449402
Majack Wariaka <majackwariaka@gmail.com>
2023-10-19 23:14:32 +0300

    betty style

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 /**
- * list_s - list
+ * struct list_s - list
  * @str: string
  * @len: length
  * @next: next
@@ -11,6 +11,6 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 size_t print_list(const list_t *h);
 #endif
