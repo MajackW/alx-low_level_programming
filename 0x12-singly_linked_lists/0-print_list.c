dc716fd49be54a998bd6c78575a3dd0d50f1dabb
Majack Wariaka <majackwariaka@gmail.com>
2023-10-19 23:01:06 +0300

    singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * print_list - prints a singly linked list
+ * @h: list to print
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t n;
+
+	n = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
