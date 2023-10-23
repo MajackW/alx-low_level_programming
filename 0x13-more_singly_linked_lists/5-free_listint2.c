e283733e3d5abcd300f5af961c3f1a75686d82df
Majack Wariaka <majackwariaka@gmail.com>
2023-10-23 21:02:56 +0300

    free the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list
+ * @head: pointer to first element
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+}
