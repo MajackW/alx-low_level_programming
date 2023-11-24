ec0a68e882402fd739cd5a8c9738e664129279a1
Majack Wariaka <majackwariaka@gmail.com>
2023-11-24 11:29:05 +0300

    sets a particular bit to 0 at a given index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include <stdio.h.
+#include "main.h"
+
+/**
+ * clear_bit - clears bit at a given index
+ * @n: int to clear
+ * @index: index to clear
+ * Return: new integer
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n &= ~(1UL << index);
+	return (1);
+}
